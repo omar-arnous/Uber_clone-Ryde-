a292df7cfb66184ea04e62056b31d9ae17d0b436
omar-arnous <omar99.arnous@gmail.com>
2024-09-21 10:10:44 +0300

    Develop the location autocomplete search bar

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,5 +1,6 @@
 import { GoogleInputProps } from "@/types/type";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const GoogleTextInput = ({
   icon,
@@ -12,7 +13,38 @@ const GoogleTextInput = ({
     <View
       className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}
     >
-      <Text>Search</Text>
+      <GooglePlacesAutocomplete
+        fetchDetails={true}
+        placeholder="Where you want to go?"
+        debounce={200}
+        styles={{
+          textInputContainer: {
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 20,
+            marginHorizontal: 20,
+            position: "relative",
+            shadowColor: "#D4D4D4",
+          },
+          textInput: {
+            backgroundColor: textInputBackgroundColor || "white",
+            fontSize: 16,
+            fontWeight: "600",
+            marginTop: 5,
+            width: "100%",
+            borderRadius: 200,
+          },
+          listView: {
+            backgroundColor: textInputBackgroundColor || "white",
+            position: "relative",
+            top: 0,
+            width: "100%",
+            borderRadius: 10,
+            shadowColor: "#D4D4D4",
+            zIndex: 99,
+          },
+        }}
+      />
     </View>
   );
 };
