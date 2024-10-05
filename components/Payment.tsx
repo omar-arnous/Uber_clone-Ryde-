bd3f231fcfe945a3334ff1aabd32a50e638aeb36
omar-arnous <omar99.arnous@gmail.com>
2024-10-05 20:18:54 +0300

    map directions and complete the main flow

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,11 +1,15 @@
 import { useStripe } from "@stripe/stripe-react-native";
 import CustomButton from "./CustomButton";
-import { Alert } from "react-native";
+import { Alert, Image, Text, View } from "react-native";
 import { useState } from "react";
 import { fetchAPI } from "@/lib/fetch";
 import { useLocationStore } from "@/store";
 import { useAuth } from "@clerk/clerk-expo";
 import { PaymentProps } from "@/types/type";
+import ReactNativeModal from "react-native-modal";
+import React from "react";
+import { images } from "@/constants";
+import { router } from "expo-router";
 
 const Payment = ({
   fullName,
@@ -28,10 +32,10 @@ const Payment = ({
 
   const initializePaymentSheet = async () => {
     const { error } = await initPaymentSheet({
-      merchantDisplayName: "",
+      merchantDisplayName: "Ryde Inc.",
       intentConfiguration: {
         mode: {
-          amount: 1099,
+          amount: parseInt(amount) * 100,
           currencyCode: "USD",
         },
         confirmHandler: async (paymentMethod, _, intentCreationCallback) => {
@@ -110,11 +114,38 @@ const Payment = ({
   };
 
   return (
-    <CustomButton
-      title="Confirm Ride"
-      className="my-10"
-      onPress={openPaymentSheet}
-    />
+    <>
+      <CustomButton
+        title="Confirm Ride"
+        className="my-10"
+        onPress={openPaymentSheet}
+      />
+
+      <ReactNativeModal
+        isVisible={success}
+        onBackdropPress={() => setSuccess(false)}
+      >
+        <View className="flex flex-col items-center justify-center bg-white p-7 rounded-2xl">
+          <Image source={images.check} className="w-28 h-28 mt-5" />
+          <Text className="text-2xl text-center font-JakartaBold mt-5">
+            Ride booked!
+          </Text>
+          <Text className="text-md text-general-200 font-JakartaMedium text-center mt-3">
+            Thank you for your booking. Your reservation has been placed. Please
+            proceed with your trip!
+          </Text>
+
+          <CustomButton
+            title="Back Home"
+            onPress={() => {
+              setSuccess(false);
+              router.push("/(root)/(tabs)/home");
+            }}
+            className="mt-5"
+          />
+        </View>
+      </ReactNativeModal>
+    </>
   );
 };
 
